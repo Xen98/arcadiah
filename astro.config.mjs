c80a31b201ffd07533cd930db850dd26189eb23c
Xen <luisgpm98@gmail.com>
2024-10-03 23:58:34 -0600

    partial: cambios de rutas gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://xen98.github.io/arcadiah/', // Tu URL de sitio
+  outDir: 'docs',
+  base: '/arcadiah/', // Cambia esto si tu sitio está en un subdirectorio
 });
